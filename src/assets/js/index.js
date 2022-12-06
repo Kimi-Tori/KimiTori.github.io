bf556eebdd48da7356f7ba50b8052ad354de2cd0
Aldushin Sergey <nn_322@mail.ru>
2022-12-06 23:23:02 +0300

    header adaptive upload

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,4 +1,20 @@
+import '../scss/style.scss';
 
-import '../scss/style.scss'
+let burgerOpen = document.getElementById("burgerOpen");
+let burgerClose = document.getElementById("burgerClose");
+let burgerBlock = document.getElementById("burgerBlock");
+let burgerNavFirst = document.getElementById("burgerNavFirst");
+let burgerNavTwo = document.getElementById("burgerNavTwo");
+let burgerMainFirst = document.getElementById("burgerMainFirst");
+let burgerMainTwo = document.getElementById("burgerMainTwo");
 
+burgerOpen.onclick = () => {
+    burgerBlock.classList.toggle("block-opened");
+}
 
+burgerNavFirst.onclick = () => {
+    burgerMainFirst.classList.toggle("block-opened");
+}
+burgerNavTwo.onclick = () => {
+    burgerMainTwo.classList.toggle("block-opened");
+}
